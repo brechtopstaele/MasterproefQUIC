brechtopstaele/MasterproefQUIC

commit bf1cdce5bb412d25b2e09cf5bbf2946367831627

Fix unaligned read access in parsing_l3.c (only for now)

src/parsing_l3.c
@@ -78,16 +78,16 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
 
   int8_t to_return = PFWL_STATUS_OK;
 
-  struct ip6_hdr *ip6 = NULL;
-  struct iphdr *ip4 = NULL;
+  struct ip6_hdr copy_ip6_hdr;
+  struct iphdr copy_ip4_hdr;
 
   if (version == PFWL_PROTO_L3_IPV4) { /** IPv4 **/
-    ip4 = (struct iphdr *) (p_pkt);
-    uint16_t tot_len = ntohs(ip4->tot_len);
+    memcpy(&copy_ip4_hdr, p_pkt, sizeof(copy_ip4_hdr));
+    uint16_t tot_len = ntohs(copy_ip4_hdr.tot_len);
 
 #ifdef PFWL_ENABLE_L3_TRUNCATION_PROTECTION
-    if (unlikely(length < (sizeof(struct iphdr)) || tot_len > length ||
-                 tot_len <= ((ip4->ihl) * 4))) {
+    if (unlikely(length < (sizeof(copy_ip4_hdr)) || tot_len > length ||
+                 tot_len <= ((copy_ip4_hdr.ihl) * 4))) {
       return PFWL_ERROR_L3_PARSING;
     }
 #endif
@@ -99,7 +99,7 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
      */
     length = tot_len;
 
-    uint16_t offset = ntohs(ip4->frag_off);
+    uint16_t offset = ntohs(copy_ip4_hdr.frag_off);
     uint8_t more_fragments;
     if (unlikely((offset & PFWL_IPv4_FRAGMENTATION_MF))) {
       more_fragments = 1;
@@ -124,22 +124,22 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
         return PFWL_STATUS_IP_FRAGMENT;
       }
       to_return = PFWL_STATUS_IP_DATA_REBUILT;
-      ip4 = (struct iphdr *) (pkt);
-      length = ntohs(((struct iphdr *) (pkt))->tot_len);
+      memcpy(&copy_ip4_hdr, pkt, sizeof(copy_ip4_hdr));
+      length = ntohs(copy_ip4_hdr.tot_len);
     } else {
       return PFWL_STATUS_IP_FRAGMENT;
     }
 
-    dissection_info->l3.addr_src.ipv4 = ip4->saddr;
-    dissection_info->l3.addr_dst.ipv4 = ip4->daddr;
+    dissection_info->l3.addr_src.ipv4 = copy_ip4_hdr.saddr;
+    dissection_info->l3.addr_dst.ipv4 = copy_ip4_hdr.daddr;
 
-    application_offset = (ip4->ihl) * 4;
+    application_offset = (copy_ip4_hdr.ihl) * 4;
     relative_offset = application_offset;
-    next_header = ip4->protocol;
+    next_header = copy_ip4_hdr.protocol;
   } else if (version == PFWL_PROTO_L3_IPV6) { /** IPv6 **/
-    ip6 = (struct ip6_hdr *) (pkt);
+    memcpy(&copy_ip6_hdr, pkt, sizeof(copy_ip6_hdr));
     uint16_t tot_len =
-        ntohs(ip6->ip6_ctlun.ip6_un1.ip6_un1_plen) + sizeof(struct ip6_hdr);
+        ntohs(copy_ip6_hdr.ip6_ctlun.ip6_un1.ip6_un1_plen) + sizeof(copy_ip6_hdr);
 #ifdef PFWL_ENABLE_L3_TRUNCATION_PROTECTION
     if (unlikely(tot_len > length)) {
       return PFWL_ERROR_L3_PARSING;
@@ -154,12 +154,12 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
      */
     length = tot_len;
 
-    dissection_info->l3.addr_src.ipv6 = ip6->ip6_src;
-    dissection_info->l3.addr_dst.ipv6 = ip6->ip6_dst;
+    dissection_info->l3.addr_src.ipv6 = copy_ip6_hdr.ip6_src;
+    dissection_info->l3.addr_dst.ipv6 = copy_ip6_hdr.ip6_dst;
 
-    application_offset = sizeof(struct ip6_hdr);
+    application_offset = sizeof(copy_ip6_hdr);
     relative_offset = application_offset;
-    next_header = ip6->ip6_ctlun.ip6_un1.ip6_un1_nxt;
+    next_header = copy_ip6_hdr.ip6_ctlun.ip6_un1.ip6_un1_nxt;
   } else {
     return PFWL_ERROR_WRONG_IPVERSION;
   }
@@ -175,11 +175,12 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
       }
 #endif
       if (likely(version == 6)) {
-        struct ip6_hbh *hbh_hdr = (struct ip6_hbh *) (pkt + application_offset);
-        tmp = (8 + hbh_hdr->ip6h_len * 8);
+        struct ip6_hbh copy_hbh_hdr;
+        memcpy(&copy_hbh_hdr, pkt + application_offset, sizeof(copy_hbh_hdr));
+        tmp = (8 + copy_hbh_hdr.ip6h_len * 8);
         application_offset += tmp;
         relative_offset += tmp;
-        next_header = hbh_hdr->ip6h_nxt;
+        next_header = copy_hbh_hdr.ip6h_nxt;
       } else {
         if (unlikely(pkt != p_pkt))
           free(pkt);
@@ -195,12 +196,12 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
       }
 #endif
       if (likely(version == 6)) {
-        struct ip6_dest *dst_hdr =
-            (struct ip6_dest *) (pkt + application_offset);
-        tmp = (8 + dst_hdr->ip6d_len * 8);
+        struct ip6_dest copy_dst_hdr;
+        memcpy(&copy_dst_hdr, pkt + application_offset, sizeof(copy_dst_hdr));
+        tmp = (8 + copy_dst_hdr.ip6d_len * 8);
         application_offset += tmp;
         relative_offset += tmp;
-        next_header = dst_hdr->ip6d_nxt;
+        next_header = copy_dst_hdr.ip6d_nxt;
       } else {
         if (unlikely(pkt != p_pkt))
           free(pkt);
@@ -216,12 +217,12 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
       }
 #endif
       if (likely(version == 6)) {
-        struct ip6_rthdr *rt_hdr =
-            (struct ip6_rthdr *) (pkt + application_offset);
-        tmp = (8 + rt_hdr->ip6r_len * 8);
+        struct ip6_rthdr copy_rt_hdr;
+        memcpy(&copy_rt_hdr, pkt + application_offset, sizeof(copy_rt_hdr));
+        tmp = (8 + copy_rt_hdr.ip6r_len * 8);
         application_offset += tmp;
         relative_offset += tmp;
-        next_header = rt_hdr->ip6r_nxt;
+        next_header = copy_rt_hdr.ip6r_nxt;
       } else {
         if (unlikely(pkt != p_pkt))
           free(pkt);
@@ -238,15 +239,15 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
 #endif
       if (likely(version == 6)) {
         if (state->ipv6_frag_state) {
-          struct ip6_frag *frg_hdr =
-              (struct ip6_frag *) (pkt + application_offset);
-          uint16_t offset = ((frg_hdr->ip6f_offlg & IP6F_OFF_MASK) >> 3) * 8;
+          struct ip6_frag copy_frg_hdr;
+          memcpy(&copy_frg_hdr, pkt + application_offset, sizeof(copy_frg_hdr));
+          uint16_t offset = ((copy_frg_hdr.ip6f_offlg & IP6F_OFF_MASK) >> 3) * 8;
           uint8_t more_fragments =
-              ((frg_hdr->ip6f_offlg & IP6F_MORE_FRAG)) ? 1 : 0;
+              ((copy_frg_hdr.ip6f_offlg & IP6F_MORE_FRAG)) ? 1 : 0;
           offset = ntohs(offset);
-          uint32_t fragment_size = ntohs(ip6->ip6_ctlun.ip6_un1.ip6_un1_plen) +
-                                   sizeof(struct ip6_hdr) - relative_offset -
-                                   sizeof(struct ip6_frag);
+          uint32_t fragment_size = ntohs(copy_ip6_hdr.ip6_ctlun.ip6_un1.ip6_un1_plen) +
+                                   sizeof(copy_ip6_hdr) - relative_offset -
+                                   sizeof(copy_frg_hdr);
 
           /**
            * If this fragment has been obtained from a
@@ -266,12 +267,12 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
            * reason we copy only the IPv6 header bytes.
            */
           pkt = pfwl_reordering_manage_ipv6_fragment(
-              state->ipv6_frag_state, (unsigned char *) ip6,
+              state->ipv6_frag_state, (unsigned char *) &copy_ip6_hdr,
               sizeof(struct ip6_hdr),
-              ((unsigned char *) ip6) + relative_offset +
-                  sizeof(struct ip6_frag),
-              fragment_size, offset, more_fragments, frg_hdr->ip6f_ident,
-              frg_hdr->ip6f_nxt, current_time, tid);
+              ((unsigned char *) &copy_ip6_hdr) + relative_offset +
+                  sizeof(copy_frg_hdr),
+              fragment_size, offset, more_fragments, copy_frg_hdr.ip6f_ident,
+              copy_frg_hdr.ip6f_nxt, current_time, tid);
 
           if (to_delete)
             free(to_delete);
@@ -282,8 +283,13 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
 
           to_return = PFWL_STATUS_IP_DATA_REBUILT;
           next_header = IPPROTO_IPV6;
-          length = ((struct ip6_hdr *) (pkt))->ip6_ctlun.ip6_un1.ip6_un1_plen +
-                   sizeof(struct ip6_hdr);
+          {
+              struct ip6_hdr tmp_ip6_hdr;
+              memcpy(&tmp_ip6_hdr, pkt, sizeof(tmp_ip6_hdr));
+
+              length = tmp_ip6_hdr.ip6_ctlun.ip6_un1.ip6_un1_plen +
+                   sizeof(tmp_ip6_hdr);
+          }
           /**
            * Force the next iteration to analyze the
            * reassembled IPv6 packet.
@@ -301,10 +307,10 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
     case IPPROTO_IPV6: /** 6in4 and 6in6 tunneling **/
       /** The real packet is now ipv6. **/
       version = 6;
-      ip6 = (struct ip6_hdr *) (pkt + application_offset);
+      memcpy(&copy_ip6_hdr, pkt + application_offset, sizeof(copy_ip6_hdr));
 #ifdef PFWL_ENABLE_L3_TRUNCATION_PROTECTION
-      if (unlikely(ntohs(ip6->ip6_ctlun.ip6_un1.ip6_un1_plen) +
-                       sizeof(struct ip6_hdr) >
+      if (unlikely(ntohs(copy_ip6_hdr.ip6_ctlun.ip6_un1.ip6_un1_plen) +
+                       sizeof(copy_ip6_hdr) >
                    length - application_offset)) {
         if (unlikely(pkt != p_pkt))
           free(pkt);
@@ -312,30 +318,30 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
       }
 #endif
 
-      dissection_info->l3.addr_src.ipv6 = ip6->ip6_src;
-      dissection_info->l3.addr_dst.ipv6 = ip6->ip6_dst;
+      dissection_info->l3.addr_src.ipv6 = copy_ip6_hdr.ip6_src;
+      dissection_info->l3.addr_dst.ipv6 = copy_ip6_hdr.ip6_dst;
 
-      application_offset += sizeof(struct ip6_hdr);
-      relative_offset = sizeof(struct ip6_hdr);
-      next_header = ip6->ip6_ctlun.ip6_un1.ip6_un1_nxt;
+      application_offset += sizeof(copy_ip6_hdr);
+      relative_offset = sizeof(copy_ip6_hdr);
+      next_header = copy_ip6_hdr.ip6_ctlun.ip6_un1.ip6_un1_nxt;
       break;
     case 4: /* 4in4 and 4in6 tunneling */
       /** The real packet is now ipv4. **/
       version = 4;
-      ip4 = (struct iphdr *) (pkt + application_offset);
+      memcpy(&copy_ip4_hdr, pkt + application_offset, sizeof(copy_ip4_hdr));
 #ifdef PFWL_ENABLE_L3_TRUNCATION_PROTECTION
-      if (unlikely(application_offset + sizeof(struct iphdr) > length ||
-                   application_offset + ((ip4->ihl) * 4) > length ||
-                   application_offset + ntohs(ip4->tot_len) > length)) {
+      if (unlikely(application_offset + sizeof(copy_ip4_hdr) > length ||
+                   application_offset + ((copy_ip4_hdr.ihl) * 4) > length ||
+                   application_offset + ntohs(copy_ip4_hdr.tot_len) > length)) {
         if (unlikely(pkt != p_pkt))
           free(pkt);
         return PFWL_ERROR_L3_PARSING;
       }
 #endif
-      dissection_info->l3.addr_src.ipv4 = ip4->saddr;
-      dissection_info->l3.addr_dst.ipv4 = ip4->daddr;
-      next_header = ip4->protocol;
-      tmp = (ip4->ihl) * 4;
+      dissection_info->l3.addr_src.ipv4 = copy_ip4_hdr.saddr;
+      dissection_info->l3.addr_dst.ipv4 = copy_ip4_hdr.daddr;
+      next_header = copy_ip4_hdr.protocol;
+      tmp = (copy_ip4_hdr.ihl) * 4;
       application_offset += tmp;
       relative_offset = tmp;
       break;

commit 15f6c26d3cf375d85237932d4ab3012622329e0f

Fix regression in testIPFragmentation
This was due to a recent regression being introduced.
Fixes: bf1cdce5bb412d25b2e09cf5bbf2946367831627

src/parsing_l3.c
@@ -81,6 +81,8 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
   struct ip6_hdr copy_ip6_hdr;
   struct iphdr copy_ip4_hdr;
 
+  const unsigned char *ip6_hdr_ptr = NULL;
+
   if (version == PFWL_PROTO_L3_IPV4) { /** IPv4 **/
     memcpy(&copy_ip4_hdr, p_pkt, sizeof(copy_ip4_hdr));
     uint16_t tot_len = ntohs(copy_ip4_hdr.tot_len);
@@ -137,6 +139,7 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
     relative_offset = application_offset;
     next_header = copy_ip4_hdr.protocol;
   } else if (version == PFWL_PROTO_L3_IPV6) { /** IPv6 **/
+    ip6_hdr_ptr = pkt;
     memcpy(&copy_ip6_hdr, pkt, sizeof(copy_ip6_hdr));
     uint16_t tot_len =
         ntohs(copy_ip6_hdr.ip6_ctlun.ip6_un1.ip6_un1_plen) + sizeof(copy_ip6_hdr);
@@ -267,9 +270,9 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
            * reason we copy only the IPv6 header bytes.
            */
           pkt = pfwl_reordering_manage_ipv6_fragment(
-              state->ipv6_frag_state, (unsigned char *) &copy_ip6_hdr,
+              state->ipv6_frag_state, ip6_hdr_ptr,
               sizeof(struct ip6_hdr),
-              ((unsigned char *) &copy_ip6_hdr) + relative_offset +
+              ip6_hdr_ptr + relative_offset +
                   sizeof(copy_frg_hdr),
               fragment_size, offset, more_fragments, copy_frg_hdr.ip6f_ident,
               copy_frg_hdr.ip6f_nxt, current_time, tid);
@@ -307,7 +310,9 @@ pfwl_status_t mc_pfwl_parse_L3_header(pfwl_state_t *state,
     case IPPROTO_IPV6: /** 6in4 and 6in6 tunneling **/
       /** The real packet is now ipv6. **/
       version = 6;
-      memcpy(&copy_ip6_hdr, pkt + application_offset, sizeof(copy_ip6_hdr));
+      ip6_hdr_ptr = pkt + application_offset;
+
+      memcpy(&copy_ip6_hdr, ip6_hdr_ptr, sizeof(copy_ip6_hdr));
 #ifdef PFWL_ENABLE_L3_TRUNCATION_PROTECTION
       if (unlikely(ntohs(copy_ip6_hdr.ip6_ctlun.ip6_un1.ip6_un1_plen) +
                        sizeof(copy_ip6_hdr) >